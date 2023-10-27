ab2645498fb7d50a3b0414ff94eaa9ff44b1be63
Gianmarco Iachella <81154210+iachella@users.noreply.github.com>
2023-10-27 18:41:41 +0200

    Create InputClass.java

---
+++ tictactoe/src/InputClass.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class InputClass {
+
+    private static final String EOL = System.lineSeparator();
+    static Scanner scanner = new Scanner(System.in);
+
+    public static String readLine(String string){
+        System.out.print(string);
+        String input = scanner.nextLine();
+        return input;
+    }
+
+    public static int readInt(String string){
+        System.out.print(string);
+        int input = scanner.nextInt();
+        scanner.nextLine();
+        return input;
+    }
+
+    public static double readDouble(String string){
+        System.out.print(string);
+        double input = scanner.nextDouble();
+        scanner.nextLine();
+        return input;
+    }
+
+
+    public static void closeScanner() {
+        scanner.close();
+    }
+}
